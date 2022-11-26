8bc8ec59d4ea95a55ec3c54c908107674ecc990b
alaskaThunderfx <smackey85@yahoo.com>
2022-11-25 23:51:28 -0500

    Add skybox, and beginning of GameOver UI
    Add skybox, alter directional light, and begin UI design, with reload
    and quit functionality on the buttons.

---
+++ Assets/UI/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void ReloadGame()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
