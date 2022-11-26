f7727012dfab5aca78824c8430fa17ab3cf5b6d4
alaskaThunderfx <smackey85@yahoo.com>
2022-11-26 01:44:15 -0500

    Add change to mouse sensitivity on zoom

--- Assets/Player/Scripts/WeaponZoom.cs
+++ Assets/Player/Scripts/WeaponZoom.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour
 {
@@ -10,11 +11,20 @@ public class WeaponZoom : MonoBehaviour
     [SerializeField]
     float amountToZoom = 10f;
 
+    [SerializeField]
+    float zoomInSensitivity = .5f;
+
     float originalZoom;
+    float originalXSensitivity;
+    float originalYSensitivity;
+    RigidbodyFirstPersonController mainController;
 
     void Start()
     {
+        mainController = GetComponent<RigidbodyFirstPersonController>();
         originalZoom = mainCamera.fieldOfView;
+        originalXSensitivity = mainController.mouseLook.XSensitivity;
+        originalYSensitivity = mainController.mouseLook.YSensitivity;
     }
 
     void Update()
@@ -27,10 +37,14 @@ public class WeaponZoom : MonoBehaviour
         if (Input.GetMouseButton(1))
         {
             mainCamera.fieldOfView = amountToZoom;
+            mainController.mouseLook.XSensitivity = zoomInSensitivity;
+            mainController.mouseLook.YSensitivity = zoomInSensitivity;
         }
         else
         {
             mainCamera.fieldOfView = originalZoom;
+            mainController.mouseLook.XSensitivity = originalXSensitivity;
+            mainController.mouseLook.YSensitivity = originalYSensitivity;
         }
     }
 }
